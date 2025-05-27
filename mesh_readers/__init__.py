0221db255b7574ef19190611e93a16f8589dc887
Emil Mammadli <56939463+pncln@users.noreply.github.com>
2025-05-27 17:47:29 +0300

    Finish refactoring
    Deleted old stuff

--- mesh_readers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package for mesh reading functionality.
-"""
-from mesh_readers.fluent_reader import FluentMeshReader
-from mesh_readers.general_reader import read_general_mesh, is_fluent_mesh, extract_patch_points
-
-__all__ = ['FluentMeshReader', 'read_general_mesh', 'is_fluent_mesh', 'extract_patch_points']
